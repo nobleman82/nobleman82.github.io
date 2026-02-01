ab6ff0465f0de29d39df779f3f92da39bac64b32
nobelman82 <mariotuerk@gmail.com>
2026-02-01 13:04:32 +0100

    - Erste Version Projekt Seite hinzugefügt - Erste Version Youtube Playlist Steite hinzugefügt. - Miniplayer für Youtube Musik hinzugefügt - Homeseite angepasst - Bootstrap Icons hinzugefügt

---
+++ GitHubWebsite/Services/MusicService.cs
@@ -0,0 +1,35 @@
+﻿namespace GitHubWebsite.Services
+{
+    public class MusicService
+    {
+        public string CurrentPlaylistId { get; private set; }
+        public bool IsPlaying { get; private set; }
+        public event Action OnChange;
+
+        public void SetPlaylist(string playlistId)
+        {
+            // Wenn die ID schon geladen ist, tu nichts -> Verhindert Doppelt-Start!
+            if (CurrentPlaylistId == playlistId) return;
+
+            CurrentPlaylistId = playlistId;
+            IsPlaying = true;
+            NotifyStateChanged();
+        }
+
+        public void TogglePlay()
+        {
+            IsPlaying = !IsPlaying;
+            NotifyStateChanged();
+        }
+
+        public void Stop()
+        {
+            IsPlaying = false;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        
+    }
+}
